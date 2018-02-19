80b68807ae5d970b7f960dbcfd44f0c01c407fd3
zzzmfps <zzz100826@gmail.com>
2018-02-19 20:02:18 +0800

    Create Valid Parentheses.py

---
+++ Python3/Valid Parentheses.py
@@ -0,0 +1,17 @@
+// 36ms, 100.0%
+class Solution:
+    def isValid(self, s):
+        tmp, right, valid = s[0], ")]}", ["()", "[]", "{}"]
+        for i in range(1, len(s)):
+            if not tmp:
+                if s[i] in right:
+                    return False
+                else:
+                    tmp = s[i]
+            elif tmp[-1] + s[i] in valid:
+                tmp = tmp[:-1]
+            else:
+                tmp += s[i]
+        if tmp: return False
+        return True
+    
