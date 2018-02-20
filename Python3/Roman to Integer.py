96a34ce41e22001fbb41d7973c0288533571fa4c
zzzmfps <zzz100826@gmail.com>
2018-02-20 11:27:51 +0800

    Update Roman to Integer.py

--- Python3/Roman to Integer.py
+++ Python3/Roman to Integer.py
@@ -1,9 +1,8 @@
 # 140ms, 100.0%
 class Solution:
     def romanToInt(self, s):
-        d = {"M": 1000, "CM": 900, "D": 500, "CD": 400,
-             "C": 100,  "XC": 90,  "L": 50,  "XL": 40,
-             "X": 10,   "IX": 9,   "V": 5,   "IV": 4,   "I": 1}
+        d = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90,
+             'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
         res = i = 0
         while i < len(s):
             if s[i: i + 2] in d:
