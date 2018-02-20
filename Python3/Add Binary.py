4c134fa33f8d9fd75823aa29af00b5f53f869789
zzzmfps <zzz100826@gmail.com>
2018-02-20 16:20:33 +0800

    Create Add Binary.py

---
+++ Python3/Add Binary.py
@@ -0,0 +1,9 @@
+# 48ms, 100.0%
+class Solution:
+    def addBinary(self, a, b):
+        if len(a) == 0: return b
+        if len(b) == 0: return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[0: -1], b[0: -1]), '1') + '0'
+        else:
+            return self.addBinary(a[0: -1], b[0: -1]) + str(int(a[-1]) + int(b[-1]))
