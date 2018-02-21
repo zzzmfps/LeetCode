b5ce32821c2dd21efdda962267425535c005649a
zzzmfps <zzz100826@gmail.com>
2018-02-21 11:13:49 +0800

    Create Binary Tree Level Order Traversal II.py

---
+++ Python3/Binary Tree Level Order Traversal II.py
@@ -0,0 +1,14 @@
+# 40ms, 100.0%
+class Solution:
+    def levelOrderBottom(self, root):
+        res = []
+        self.dfs(root, 0, res)
+        return res
+
+    def dfs(self, root, level, res):
+        if root:
+            if len(res) < level + 1:
+                res.insert(0, [])
+            res[-(level + 1)].append(root.val)
+            self.dfs(root.left, level + 1, res)
+            self.dfs(root.right, level + 1, res)
