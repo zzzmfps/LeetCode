2662a867673276b0a4424c9c417793112137d177
zzzmfps <zzz100826@gmail.com>
2018-02-21 14:05:47 +0800

    Update Binary Tree Level Order Traversal II.py

--- Python3/Binary Tree Level Order Traversal II.py
+++ Python3/Binary Tree Level Order Traversal II.py
@@ -5,9 +5,9 @@ class Solution:
         self.dfs(root, 0, res)
         return res
 
-    def dfs(self, root, level, res):
-        if root:
-            if len(res) < level + 1:
+    def dfs(self, root, level, res):    # much faster to put this function
+        if root:                        # outside the function levelOrderBottom
+            if len(res) < level + 1:    # def inside will cost 52~84ms
                 res.insert(0, [])
             res[-(level + 1)].append(root.val)
             self.dfs(root.left, level + 1, res)
