91768ac9648b3b04bc4cabca9917eece9be23afa
zzzmfps <zzz100826@gmail.com>
2018-02-24 10:43:53 +0800

    Update Symmetric Tree.py

--- Python3/Symmetric Tree.py
+++ Python3/Symmetric Tree.py
@@ -1,12 +1,13 @@
 # 72ms, 46.55%
 class Solution:
+    
     def isSymmetric(self, root):
         if not root: return True
-        return self.helper(root.left, root.right)
+        return self.is_sym(root.left, root.right)
     
-    def helper(self, l, r):
+    def is_sym(self, l, r):
         if not l and not r: return True
         if l and r and l.val == r.val:
-            return self.helper(l.left, r.right) and self.helper(l.right, r.left)
+            return self.is_sym(l.left, r.right) and self.is_sym(l.right, r.left)
         return False
         
