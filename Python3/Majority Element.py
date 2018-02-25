7b5b0c87197d47443dcf1cdbc79633601796395f
zzzmfps <zzz100826@gmail.com>
2018-02-25 11:56:00 +0800

    Create Majority Element.py

---
+++ Python3/Majority Element.py
@@ -0,0 +1,17 @@
+# 46ms, 87.17%
+class Solution(object):
+    def majorityElement(self, nums):
+        cur, count = nums[0], 0
+        for num in nums:
+            if cur == num:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    cur, count = num, 1
+        return cur
+        '''
+        45ms, 89.88%
+        return sorted(nums)[len(nums) / 2]
+        '''
+        
