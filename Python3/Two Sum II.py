dee94eaa7e0747f818d5343401fdc981c5baa2c1
zzzmfps <zzz100826@gmail.com>
2018-02-25 14:38:14 +0800

    Update Two Sum II.py

--- Python3/Two Sum II.py
+++ Python3/Two Sum II.py
@@ -1,7 +1,7 @@
-# 40ms, 37.57%
-class Solution(object):
+# 40ms, 100.0%
+class Solution:
     def twoSum(self, numbers, target):
-        rem = dict()
+        rem = {}
         for i in range(len(numbers)):
             if numbers[i] in rem: return [rem[numbers[i]] + 1, i + 1]
             rem[target - numbers[i]] = i
