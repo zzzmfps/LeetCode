5e432aa4735c0c89272d03d3934ea5deede92912
zzzmfps <zzz100826@gmail.com>
2018-02-26 23:05:47 +0800

    Update Count Primes.py

--- Python3/Count Primes.py
+++ Python3/Count Primes.py
@@ -6,12 +6,10 @@ class Solution:
         for i in range(2, int(n ** 0.5) + 1):
             if primes[i]:
                 square = i * i
-                '''
-                [a: b: c]: interval [a, b), every c elements
-                eg. [1: 9: 2]: 1, 3, 5, 7
-                so, [i * i: n: i] --> [((i * i): n): i]: i * i, i * (i + 1), ... (< n)
-                (n - square - 1) // i + 1 == len(primes[square: n: i])
-                '''
+                # [a: b: c]: interval [a, b), every c elements
+                # eg. [1: 9: 2]: 1, 3, 5, 7
+                # so, [(square: n): i]: i * i, i * (i + 1), ... (< n)
+                # (n - square - 1) // i + 1 == len(primes[square: n: i])
                 primes[square: n: i] = [False] * ((n - square - 1) // i + 1)
         return sum(primes)
     
