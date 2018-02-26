dfd1ffba4438af3ec4bc6763be36b5fa78aee4ae
zzzmfps <zzz100826@gmail.com>
2018-02-26 23:12:25 +0800

    Update Count Primes.py

--- Python3/Count Primes.py
+++ Python3/Count Primes.py
@@ -1,8 +1,8 @@
-# 240ms, 100.0%
+# 168ms, 100.0%
 class Solution:
     def countPrimes(self, n):
-        primes = [True] * n
-        primes[: 2] = [False] * 2
+        primes = [1] * n    # use 0, 1 instead of True, False will be much faster
+        primes[: 2] = [0] * 2    # avoid checking whether n is less than 3
         for i in range(2, int(n ** 0.5) + 1):
             if primes[i]:
                 square = i * i
@@ -10,6 +10,6 @@ class Solution:
                 # eg. [1: 9: 2]: 1, 3, 5, 7
                 # so, [(square: n): i]: i * i, i * (i + 1), ... (< n)
                 # (n - square - 1) // i + 1 == len(primes[square: n: i])
-                primes[square: n: i] = [False] * ((n - square - 1) // i + 1)
+                primes[square: n: i] = [0] * ((n - square - 1) // i + 1)
         return sum(primes)
     
