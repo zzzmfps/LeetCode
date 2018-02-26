fa4a082596d3b356ae871ea8f4c4c402a01a555a
zzzmfps <zzz100826@gmail.com>
2018-02-26 23:17:46 +0800

    Update Count and Say.py

--- Python3/Count and Say.py
+++ Python3/Count and Say.py
@@ -2,7 +2,6 @@
 class Solution:
     def countAndSay(self, n):
         s = '1'
-        for _ in range(n - 1):    // not really understand
+        for _ in range(n - 1):    # do not really understand
             s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
         return s
-        
