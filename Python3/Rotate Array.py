31ff853bc0e5032eb897de1e02c3dad65e3be988
zzzmfps <zzz100826@gmail.com>
2018-02-26 23:24:36 +0800

    Update Rotate Array.py

--- Python3/Rotate Array.py
+++ Python3/Rotate Array.py
@@ -1,13 +1,5 @@
 # 56ms, 100.0%
 class Solution:
     def rotate(self, nums, k):
-        length = len(nums)
-        k %= length
-        n = length - k
-        nums.extend(nums[: n])
-        nums[: n] = []
-        '''
-        ### 56ms, 100.0%
         k %= len(nums)
         if k != 0: nums[: 0], nums[-k: ] = nums[-k: ], []
-        '''
