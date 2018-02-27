38a877b28d86789c5fcdc4031ba66df237fc053c
zzzmfps <zzz100826@gmail.com>
2018-02-27 22:52:16 +0800

    Create Delete Node in a Linked List.py

---
+++ Python3/Delete Node in a Linked List.py
@@ -0,0 +1,9 @@
+# 46ms, 51.08%
+class Solution(object):
+    def deleteNode(self, node):
+    '''This function cannot delete the last node.
+    It is NOT the real deletion.
+    '''
+        node.val = node.next.val
+        node.next = node.next.next
+        
