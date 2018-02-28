e3aae6dd142c4e424b2a3033bceed85c3a885336
zzzmfps <zzz100826@gmail.com>
2018-02-28 22:47:24 +0800

    Update Move Zeroes.cc

--- Cpp/Move Zeroes.cc
+++ Cpp/Move Zeroes.cc
@@ -3,8 +3,8 @@ class Solution {
 public:
     void moveZeroes(vector<int> &nums) {
         int ins_index = 0, end = nums.size();
-        while (nums[ins_index] != 0) ++ins_index;
-        int trav = ins_index + 1;
+        while (nums[ins_index] != 0) ++ins_index;    // when vector nums[ins_index] out of range, it returns 0
+        int trav = ins_index + 1;                    // vector<int> v{1, 1}; --> v[2] returns 0
         while (trav < end) {
             if (nums[trav] != 0)
                 swap(nums[trav], nums[ins_index++]);
