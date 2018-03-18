eddd65658b46a2b367302237b6c465dd3371eba5
zzzmfps <zzz100826@gmail.com>
2018-03-18 18:12:52 +0800

    Create Intersection of Two Arrays II.cc

---
+++ Cpp/Intersection of Two Arrays II.cc
@@ -0,0 +1,21 @@
+// 8ms, 70.61%
+class Solution {
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
+        unordered_map<int, int> um;
+        for (auto x : nums1) {
+            if (um.find(x) != um.end())
+                um[x] += 1;
+            else
+                um[x] = 1;
+        }
+        vector<int> res;
+        for (auto y : nums2) {
+            if (um.find(y) != um.end() && um[y]) {
+                res.push_back(y);
+                um[y] -= 1;
+            }
+        }
+        return res;
+    }
+};
