8439f38085293a4a359e4e98c5c2293b756bbafc
zzzmfps <zzz100826@gmail.com>
2018-03-21 18:14:09 +0800

    Create Sum of Left Leaves.py

---
+++ Python3/Sum of Left Leaves.py
@@ -0,0 +1,8 @@
+# 48ms, 71.47%
+class Solution:
+    def sumOfLeftLeaves(self, root):
+        if not root: return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+    
