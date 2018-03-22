eec34414803c119aa93a1560c24ce16ed1e7fc6e
zzzmfps <zzz100826@gmail.com>
2018-03-22 13:52:19 +0800

    Create Third Maximum Number.py

---
+++ Python3/Third Maximum Number.py
@@ -0,0 +1,12 @@
+# 44ms, 85.00%
+class Solution:
+    def thirdMax(self, nums):
+        n_inf = float('-inf')
+        v = [n_inf, n_inf, n_inf]
+        for num in nums:
+            if num not in v:
+                if num > v[0]: v = [num, v[0], v[1]]
+                elif num > v[1]: v = [v[0], num, v[1]]
+                elif num > v[2]: v = [v[0], v[1], num]
+        return v[0] if n_inf in v else v[2]
+    
