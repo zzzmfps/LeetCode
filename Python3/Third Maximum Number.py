17a66707ffc52d9ce876a5e7443ae49a88544c82
zzzmfps <zzz100826@gmail.com>
2018-03-22 13:52:57 +0800

    Update Third Maximum Number.py

--- Python3/Third Maximum Number.py
+++ Python3/Third Maximum Number.py
@@ -5,8 +5,11 @@ class Solution:
         v = [n_inf, n_inf, n_inf]
         for num in nums:
             if num not in v:
-                if num > v[0]: v = [num, v[0], v[1]]
-                elif num > v[1]: v = [v[0], num, v[1]]
-                elif num > v[2]: v = [v[0], v[1], num]
+                if num > v[0]:
+                    v = [num, v[0], v[1]]
+                elif num > v[1]:
+                    v = [v[0], num, v[1]]
+                elif num > v[2]:
+                    v = [v[0], v[1], num]
         return v[0] if n_inf in v else v[2]
     
