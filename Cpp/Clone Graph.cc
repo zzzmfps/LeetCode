c93f3fd37c89ed54fb7eafb96c8c6847e0d72242
zzzmfps <zzz100826@gmail.com>
2018-04-04 18:33:02 +0800

    Create Clone Graph.cc

---
+++ Cpp/Clone Graph.cc
@@ -0,0 +1,24 @@
+// 32ms, 37.30%
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+private:
+    unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> hash;
+    
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if (!node) return node;
+        if (hash.find(node) == hash.end()) {
+            hash[node] = new UndirectedGraphNode(node->label);
+            for (auto x : node->neighbors)
+                (hash[node]->neighbors).push_back(cloneGraph(x));
+        }
+        return hash[node];
+    }
+};
