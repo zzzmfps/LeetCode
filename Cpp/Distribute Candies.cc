e9c82500d454d0c28fd908def235d418c054bf80
zzzmfps <zzz100826@gmail.com>
2018-04-05 21:41:39 +0800

    Create Distribute Candies.cc

---
+++ Cpp/Distribute Candies.cc
@@ -0,0 +1,14 @@
+// 202ms, 96.02%
+class Solution {
+public:
+    int distributeCandies(vector<int> &candies) {
+        int half = candies.size() / 2, kinds = 0;
+        int kind[200001] = {};
+        for (auto c : candies)
+            if (kind[c + 100000] == 0) {
+                kind[c + 100000] += 1;
+                kinds += 1;
+            }
+        return min(half, kinds);
+    }
+};
