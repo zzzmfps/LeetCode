0282b0650d4d41603ca8c4470f45e3bb7e7be54c
zzzmfps <zzz100826@gmail.com>
2018-04-06 16:39:57 +0800

    Update Partition Equal Subset Sum.cc

--- Cpp/Partition Equal Subset Sum.cc
+++ Cpp/Partition Equal Subset Sum.cc
@@ -5,7 +5,9 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum & 1) return false;
         sum >>= 1;
-        bool dp[sum + 1] = {1};
+        // bool dp[sum + 1] = {1};    // valid in C99 standard
+        bool *dp = new bool[sum + 1]();    // add '()' to initialize
+        dp[0] = true;
         for (auto n : nums)
             for (int i = sum; i >= n; --i)
                 dp[i] |= dp[i - n];
