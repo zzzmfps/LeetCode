5fb313649c691f7d9e1a47d419f6c074d22d1e10
zzzmfps <zzz100826@gmail.com>
2018-04-13 09:43:02 +0800

    Update Reverse Words in a String III.cc

--- Cpp/Reverse Words in a String III.cc
+++ Cpp/Reverse Words in a String III.cc
@@ -1,4 +1,10 @@
-// 25ms, 52.53%
+// 13ms, 99.30%
+static int x = []() {
+    ios_base::sync_with_stdio(false); // toggles off the synchronization
+    cin.tie(nullptr);                 // ties cin with nullptr, not cout
+    return 0;
+}();
+
 class Solution
 {
   public:
@@ -10,7 +16,7 @@ class Solution
         istringstream iss(s);
         while (true)
         {
-            iss >> temp;
+            iss >> temp; // input
             res += string(temp.rbegin(), temp.rend());
             if (iss.eof())
                 return res;
