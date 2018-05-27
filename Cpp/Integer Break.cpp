a23f1c53e1dcca37515abc2fa2fe9cf93f1ec9e0
zzzmfps <zzz100826@gmail.com>
2018-05-27 09:37:08 +0800

    Create Integer Break.cpp

---
+++ Cpp/Integer Break.cpp
@@ -0,0 +1,23 @@
+// 2ms, 100.0%
+static int x = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    int integerBreak(int n)
+    {
+        int maxProduct = 1;
+        for (int i = max(2, int(sqrt(n))); i < max(3, (n + 1) / 2); ++i) {
+            int num = n / i, cur = num;
+            int tmp1 = n % i, tmp2 = i - tmp1 - 1;
+            while (tmp1--) cur *= num + 1;
+            while (tmp2--) cur *= num;
+            maxProduct = max(maxProduct, cur);
+        }
+        return maxProduct;
+    }
+};
