28f49f74a9d38ca60c6fc97b5414668a8cf32ecf
zzzmfps <zzz100826@gmail.com>
2018-08-15 22:47:15 +0800

    Create Mirror Reflection.cpp

---
+++ Cpp/Mirror Reflection.cpp
@@ -0,0 +1,10 @@
+// 0ms, 100.0%
+class Solution
+{
+  public:
+    int mirrorReflection(int p, int q)
+    {
+        while (!(p & 1 || q & 1)) p >>= 1, q >>= 1;
+        return 1 - (p & 1) + (q & 1);
+    }
+};
