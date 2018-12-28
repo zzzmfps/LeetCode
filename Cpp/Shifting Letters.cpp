c2e35209e09c72bd258c39c109d1bfd4d7ab245c
zzzmfps <zzz100826@gmail.com>
2018-12-28 20:29:41 +0800

    Update Shifting Letters.cpp

--- Cpp/Shifting Letters.cpp
+++ Cpp/Shifting Letters.cpp
@@ -1,7 +1,7 @@
 // 16ms, 99.02%
 static int x = []() {
     ios_base::sync_with_stdio(false);
-    // cin.tie(nullptr);
+    cin.tie(nullptr);
     return 0;
 }();
 
@@ -10,12 +10,12 @@ class Solution
   public:
     string shiftingLetters(const string &S, vector<int> &shifts)
     {
-        int size = shifts.size() - 1;
-        shifts[size] %= 26;
-        for (int i = size - 1; i >= 0; --i) shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
+        int size = shifts.size();
+        shifts[size - 1] %= 26;
+        for (int i = size - 2; i >= 0; --i) shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
         string res = "";
-        for (int i = 0; i <= size; ++i) {
-            int y = S[i] + shifts[i] % 26;
+        for (int i = 0; i < size; ++i) {
+            int y = S[i] + shifts[i];
             res += char(y > 122 ? y - 26 : y);
         }
         return res;
