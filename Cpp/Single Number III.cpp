87e0c3b8aae62434a0ecb764b8c7b57288307a01
zzzmfps <zzz100826@gmail.com>
2019-01-01 20:54:00 +0800

    Create Single Number III.cpp

---
+++ Cpp/Single Number III.cpp
@@ -0,0 +1,19 @@
+// 4ms, 100.0%
+static int x = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    vector<int> singleNumber(const vector<int> &nums)
+    {
+        int diff = accumulate(nums.begin(), nums.end(), 0, [](int x, int y) { return x ^ y; });
+        diff &= -diff;
+        vector<int> rets{0, 0};
+        for (const auto n : nums) rets[(n & diff) == 0 ? 0 : 1] ^= n;
+        return rets;
+    }
+};
