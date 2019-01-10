9f5999372faf8f479999306c20f0446156accadc
zzzmfps <zzz100826@gmail.com>
2019-01-10 18:46:45 +0800

    Create Rotate Function.py

---
+++ Python3/Rotate Function.py
@@ -0,0 +1,15 @@
+# 88ms
+class Solution:
+    def maxRotateFunction(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        res, sums, size = 0, sum(A), len(A)
+        for i in range(size):
+            res += i * A[i]
+        tmp = res
+        for i in range(size - 1):
+            tmp = tmp - sums + size * A[i]
+            res = max(res, tmp)
+        return res
