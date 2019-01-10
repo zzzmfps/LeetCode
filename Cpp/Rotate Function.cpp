f2eeaeb0bff22f3e007b0c52f503a6b859d5ec56
zzzmfps <zzz100826@gmail.com>
2019-01-10 19:41:47 +0800

    Create Rotate Function.cpp

---
+++ Cpp/Rotate Function.cpp
@@ -0,0 +1,22 @@
+// 4ms
+static int x = []() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	return 0;
+}();
+
+class Solution
+{
+public:
+	int maxRotateFunction(const vector<int>& A)
+	{
+		int res = 0, sum = accumulate(A.begin(), A.end(), 0), size = A.size();
+		for(int i = 0; i < size; ++i) res += i * A[i];
+		int tmp = res;
+		for(int i = 0; i < size - 1; ++i) {
+			tmp = tmp - sum + size * A[i];
+			res = max(res, tmp);
+		}
+		return res;
+	}
+};
