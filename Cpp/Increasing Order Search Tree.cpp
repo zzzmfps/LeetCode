99d635182b2f39aca99b4e2502c703d4cb52fcaa
zzzmfps <zzz100826@gmail.com>
2019-02-15 19:54:07 +0800

    Create Increasing Order Search Tree.cpp

---
+++ Cpp/Increasing Order Search Tree.cpp
@@ -0,0 +1,38 @@
+// 56ms, 100.0%; 25.3MB, 100.0%
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+static int x = []() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	return 0;
+}();
+
+class Solution
+{
+private:
+	TreeNode *head, *track;
+
+public:
+	Solution()
+	{
+		head = new TreeNode(0);
+		track = head;
+	}
+
+	TreeNode *increasingBST(TreeNode *root)
+	{
+		if(!root) return nullptr;
+		increasingBST(root->left);
+		track->right = new TreeNode(root->val);
+		track = track->right;
+		increasingBST(root->right);
+		return head->right;
+	}
+};
