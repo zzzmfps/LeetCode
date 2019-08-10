dfafb9d0f6ca73b1ed7469f841753a91ff9c8599
zzzmfps <zzz100826@gmail.com>
2019-08-10 13:35:59 +0800

    Update Car Fleet.py

--- Python3/Car Fleet.py
+++ Python3/Car Fleet.py
@@ -1,13 +1,12 @@
-# 204ms, 83.72%; 16.4MB, 50.00%
+# 196ms, 96.90%; 16.2MB, 50.00%
 class Solution:
     def carFleet(self, target: int, position: list, speed: list) -> int:
         tmp = sorted(zip(position, speed), reverse=1)
-        time = [(target - x[0]) / x[1] for x in tmp]
         res, last = 0, 0
-        for i in range(len(time)):
-            if time[i] <= last:
-                time[i] = last
-            else:
+        for i in range(len(tmp)):
+            time = (target - tmp[i][0]) / tmp[i][1]
+            if time > last:
                 res += 1
-                last = time[i]
+                last = time
         return res
+    
