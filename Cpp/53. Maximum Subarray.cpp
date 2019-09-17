95b73e129d20bc6d82cc7448740389925dae0760
zzzmfps <zzz100826@gmail.com>
2019-09-17 23:08:59 +0800

    Update and rename Maximum Subarray.cpp to 53. Maximum Subarray.cpp

---
+++ Cpp/53. Maximum Subarray.cpp
@@ -0,0 +1,14 @@
+// 8ms, 71.54%; 9.2MB, 93.14%
+class Solution
+{
+public:
+    int maxSubArray(const vector<int> &nums)
+    {
+        int tmp = nums[0], res = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            tmp = (tmp > 0 ? tmp + nums[i] : nums[i]);
+            if (tmp > res) res = tmp;
+        }
+        return res;
+    }
+};
