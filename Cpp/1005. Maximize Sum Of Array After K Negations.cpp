28d5e1975a4e83f044980c7081cb61d297cc9862
zzzmfps <zzz100826@gmail.com>
2019-10-01 16:29:27 +0800

    Create 1005. Maximize Sum Of Array After K Negations.cpp

---
+++ Cpp/1005. Maximize Sum Of Array After K Negations.cpp
@@ -0,0 +1,31 @@
+// 8ms, 65.85%; 9.3MB, 7.14%
+class Solution
+{
+public:
+    int largestSumAfterKNegations(const vector<int> &A, int K)
+    {
+        multiset<int> tmp;
+        int res = 0, _min = 0x7FFFFFFF;
+        for (auto x : A) {
+            res += x;
+            if (x <= 0) tmp.insert(x);
+            _min = min(_min, abs(x));
+        }
+        if (tmp.size() >= K) {
+            auto it = tmp.begin();
+            for (int i = 0; i < K; ++i) {
+                res -= 2 * (*it);
+                ++it;
+            }
+        } else {
+            int x = K - tmp.size();
+            auto it = tmp.begin();
+            while (it != tmp.end()) {
+                res -= 2 * (*it);
+                ++it;
+            }
+            if (x & 1) res -= 2 * _min;
+        }
+        return res;
+    }
+};
