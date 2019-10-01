df420d1d5ebcd07069c93605c155aa476278cff8
zzzmfps <zzz100826@gmail.com>
2019-10-01 20:13:06 +0800

    Create 402. Remove K Digits.cpp

---
+++ Cpp/402. Remove K Digits.cpp
@@ -0,0 +1,16 @@
+// 24ms, 23.29%; 8.9MB, 100.00%
+class Solution
+{
+public:
+    string removeKdigits(string num, int k)
+    {
+        if (num.size() == k) return "0";
+        while (k--) {
+            auto it = prev(is_sorted_until(num.begin(), num.end()));
+            num.erase(it);
+        }
+        auto it = num.find_first_not_of('0');
+        if (it == string::npos) return "0";
+        return num.substr(it);
+    }
+};
