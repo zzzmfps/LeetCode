2c93907fd7182a61a80a0067bba5b591c871f080
zzzmfps <zzz100826@gmail.com>
2020-01-06 14:38:51 +0800

    Create Reverse Only Letters.py

---
+++ Python3/Reverse Only Letters.py
@@ -0,0 +1,15 @@
+# 28ms, 63.04%; 12.5MB, 100.00%
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        S = list(S)
+        i, j = 0, len(S) - 1
+        while i < j:
+            if not S[i].isalpha():
+                i += 1
+            elif not S[j].isalpha():
+                j -= 1
+            else:
+                S[i], S[j] = S[j], S[i]
+                i += 1
+                j -= 1
+        return ''.join(S)
