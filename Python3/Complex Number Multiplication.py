22e72202e36c368ff607855a5e6ee9e54c30e179
Junhao Zhao <zzz100826@gmail.com>
2020-02-16 11:52:03 +0800

    Update Complex Number Multiplication.py

--- Python3/Complex Number Multiplication.py
+++ Python3/Complex Number Multiplication.py
@@ -1,7 +1,7 @@
-# 36ms, 13.25%; 12.7MB, 100.00%
+# 36ms, 13.25%; 12.6MB, 100.00%
 class Solution:
     def complexNumberMultiply(self, a: str, b: str) -> str:
         a1, a2 = map(int, a.rstrip('i').split('+'))
         b1, b2 = map(int, b.rstrip('i').split('+'))
-        x, y = str(a1 * b1 - a2 * b2), str(a1 * b2 + a2 * b1)
-        return x + '+' + y + 'i'
+        x, y = a1 * b1 - a2 * b2, a1 * b2 + a2 * b1
+        return f'{x}+{y}i'
