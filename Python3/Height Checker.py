c9d7d008fcc0bb4e16ffc5f40da273bb9f1dd11d
Junhao Zhao <zzz100826@gmail.com>
2020-03-11 12:39:16 +0800

    Update Height Checker.py

--- Python3/Height Checker.py
+++ Python3/Height Checker.py
@@ -1,6 +1,22 @@
+# 0, brute force, T(n) = O(nlogn)
 # 36ms, 45.68%; 12.8MB, 100.00%
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
         heights2 = sorted(heights)
         return sum([heights[i] != heights2[i] for i in range(len(heights))])
         
+
+# 1, based on count-sort, T(n) = O(n)
+# 32ms, 77.12%; 12.9MB, 100.00%, not obviously enough
+from collections import Counter
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        res, idx = 0, 0
+        for val, count in sorted(Counter(heights).items()):
+            while count:
+                count -= 1
+                res += (val != heights[idx])
+                idx += 1
+        return res
+    
