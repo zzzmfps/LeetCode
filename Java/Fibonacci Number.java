21338bfea90b1026b2659ac0f40e44a2ef03f4e8
Junhao Zhao <zzz100826@gmail.com>
2020-03-19 18:32:40 +0800

    Update Fibonacci Number.java

--- Java/Fibonacci Number.java
+++ Java/Fibonacci Number.java
@@ -1,13 +1,15 @@
 // 0ms, 100.00%; 36.6MB, 5.51%
 class Solution {
     public int fib(int N) {
-        if (N < 2)
+        if (N < 2) {
             return N;
+        }
         int[] a = new int[N + 1];
         a[0] = 0;
         a[1] = 1;
-        for (int i = 2; i <= N; ++i)
+        for (int i = 2; i <= N; ++i) {
             a[i] = a[i - 1] + a[i - 2];
+        }
         return a[N];
     }
 }
