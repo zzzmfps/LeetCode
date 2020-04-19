6567681fc678e131b1489d76572f59e0f64b93be
Junhao Zhao <zzz100826@gmail.com>
2020-04-19 20:27:08 +0800

    Create Valid Palindrome.go

---
+++ Go/Valid Palindrome.go
@@ -0,0 +1,23 @@
+// 0ms, 100.00%; 2.7MB, 100.00%
+func isPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; ; i, j = i+1, j-1 {
+		for ; i < len(s) && isNotAlpha(s[i]); i++ {
+		}
+		for ; j > -1 && isNotAlpha(s[j]); j-- {
+		}
+		if i >= j {
+			break
+		}
+		if (s[i] | 32) != (s[j] | 32) { // to lowercase
+			return false
+		}
+	}
+	return true
+}
+
+func isNotAlpha(r byte) bool {
+	// numeric: 48 ~ 57
+	// alpha, uppercase: 65 ~ 90
+	// alpha, lowercase: 97 ~ 122
+	return r < 48 || r > 57 && r < 65 || r > 90 && r < 97 || r > 122
+}
