cdb42e323ca2d3bdb30b438604e445f28a6f7fa5
Junhao Zhao <zzz100826@gmail.com>
2020-04-19 20:42:34 +0800

    Create Linked List Cycle.go

---
+++ Go/Linked List Cycle.go
@@ -0,0 +1,18 @@
+// 4ms, 98.60%; 3.8MB, 42.86%
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
