35a46fd529199f27fda56e274de23b18277422fb
Junhao Zhao <zzz100826@gmail.com>
2020-05-07 10:54:41 +0800

    Update Rotate Array.java

--- Java/Rotate Array.java
+++ Java/Rotate Array.java
@@ -1,11 +1,11 @@
-// 0ms, 100.00%; 39.8MB, 5.77%
+// 0ms, 100.00%; 39.5MB, 5.77%
 class Solution {
     public static void rotate(int[] nums, int k) {
         if ((k %= nums.length) == 0) {
             return;
         }
         int beg = 0, last = nums[0];
-        for (int i = k, j = 1; i != beg || j != nums.length; i = (i + k) % nums.length, ++j) {
+        for (int i = k, j = 1; j != nums.length; i = (i + k) % nums.length, ++j) {
             int tmp = nums[i];
             nums[i] = last;
             last = tmp;
